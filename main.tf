81939f074b53a4d0319dac0cefff833008a444f7
Alberto Rodriguez Garcia <agarcia@schubergphilis.com>
2020-05-11 22:20:58 +0200

    WIP

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ data "helm_repository" "repository" {
 resource "helm_release" "release" {
   name      = var.name
   namespace = var.namespace
-  chart     = "${var.helm_repository}/${var.name}"
+  chart     = "${var.helm_repository}/${var.chart}"
   version   = var.chart_version
   values    = [var.helm_values]
   timeout   = var.helm_timeout
